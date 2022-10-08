e4b3c7aaf4a0677c97cc86e99b677d4def4e744a
Menessaur <menegat.marcelo@gmail.com>
2022-10-08 03:54:18 +0100

    add battle screen

---
+++ gurigames/src/Components/BattleScreen.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './battleScreen.css'
+
+const BattleScreen = () => {
+    return (
+        <>
+            <div className='arena'>
+                <h1>TESTE</h1>
+                <div className='playerOne'>
+                </div>
+                <div className='playerTwo'>
+                </div>
+
+                <div className='actionPanel'>
+                </div>
+            </div>
+        </>        
+    );
+}
+
+export default BattleScreen;
